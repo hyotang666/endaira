76bbc4e4925be45ffc1788f7bfa8b9b8e8feb889
Shinichi Sato <hyotang.karakoma@gmail.com>
2017-07-30 04:50:23 +0900

    Add: Some tests.

--- spec/endaira.dsl.lisp
+++ spec/endaira.dsl.lisp
@@ -7,42 +7,63 @@
 
 ;;;; Description:
 ; Set named handler binding.
-;   syntax (WITH-HANDLER-NAMED name &BODY body)
-;   name = (OR identifier params)
-;   identifier = ATOM
-;   params = (CONS IDENTIFIER vars)
-;   vars = SYMBOL*
-;   body = S-EXPRESSION*
 
 #+syntax
 (WITH-HANDLER-NAMED name &body body) ; => result
 
+#?(with-handler-named 0 1)
+:expanded-to
+(let((var(loop :for arg :in 'nil
+	       :for val :in (list)
+	       :collect (list arg (if *copy*
+				    `(quote ,(copy val))
+				    val)))))
+  (handler-bind((error(lambda(condition)
+			(declare(ignore condition))
+			(push (make-situation :name '0
+					      :form '1
+					      :binds var)
+			      *acc*))))
+    1))
+
 ;;;; Arguments and Values:
 
-; name := 
+; name := [ identifier | parameters ]
+; identifier := atom
+; parameters := (identifier var*)
+; var := symbol
 
-; body := 
+; body := implicit progn
 
-; result := 
+; result := return value of BODY.
 
 ;;;; Affected By:
+;;; Compile time
+; none
+;;; Run time
+; `*COPY*`
 
 ;;;; Side-Effects:
+;;; Run time
+; Storing `SITUATION` object to `*ACC*` when error is signaled.
 
 ;;;; Notes:
+; When `*COPY*` is non-NIL value, `VAL` is deeply copied.
+; This means, when `VAL` is circle structure, `COPY` never return and becomes stack overflow.
 
 ;;;; Exceptional-Situations:
 
 (requirements-about +BINDS+)
 
 ;;;; Description:
+; Internal use for variable injection.
+; See #P"builder/endaira-builder.lisp" especially `BREAK` as example.
 
 ;;;; Value type is SYMBOL
-;#? +BINDS+ :be-the ???
+#? +BINDS+ :be-the symbol
 
 ; Initial value is #:BINDS
 
 ;;;; Affected By:
 
 ;;;; Notes:
-
--- spec/endaira.internals.lisp
+++ spec/endaira.internals.lisp
@@ -6,32 +6,42 @@
 (requirements-about *ACC*)
 
 ;;;; Description:
+; Acculate `SITUATION`s.
 
 ;;;; Value type is NULL
-;#? *ACC* :be-the ???
+#? *ACC* :be-the LIST
 
 ; Initial value is NIL
 
 ;;;; Affected By:
+; `endaira.dsl:with-handler-named` works as setter.
+
+; `endaira.debugger:debugger-hook` works as cleaner.
 
 ;;;; Notes:
 
 (requirements-about *CURRENT-PACKAGE*)
 
 ;;;; Description:
+; Storing current package.
 
 ;;;; Value type is PACKAGE
-;#? *CURRENT-PACKAGE* :be-the ???
+#? *CURRENT-PACKAGE* :be-the package
 
 ; Initial value is #<PACKAGE "ENDAIRA.INTERNALS">
 
 ;;;; Affected By:
+; `endaira.debugger:debugger-hook`
 
 ;;;; Notes:
+; For ECL.
+; ECL deubgger binds `*package*` with cl-user in debugger.
 
 (requirements-about QUIT-SIGNAL)
 
 ;;;; Description:
+; Internal use for quit endaira debug repl.
+
 ;;;; Class Precedence List: (case in SBCL)
 ; quit-signal condition slot-object t
 
@@ -42,27 +52,36 @@
 (requirements-about SITUATION)
 
 ;;;; Description:
+; Consider this is something like stack frame.
+
 ;;;; Class Precedence List: (case in SBCL)
 ; situation structure-object slot-object t
 
 ;;;; Effective Slots:
 
 ; NAME [Type] (OR SYMBOL STRING (CONS (EQL SETF) T))
+; Situation name.
 
 ; BINDS [Type] LIST
+; Variable bindings if any.
 
 ; FORM [Type] T
+; S-Expression as stack frame.
 
 ;;;; Notes:
 
 (requirements-about MAKE-SITUATION)
 
 ;;;; Description:
+; Constructor
 
 #+syntax
-(MAKE-SITUATION &key ((:name #:name) nil) ((:binds #:binds) nil) ((:form
-                                                                   #:form)
-                                                                  nil)) ; => result
+(MAKE-SITUATION &key
+		((:name #:name) nil)
+		((:binds #:binds) nil)
+		((:form #:form) nil))
+; => result
+#?(make-situation) :be-the situation
 
 ;;;; Arguments and Values:
 
